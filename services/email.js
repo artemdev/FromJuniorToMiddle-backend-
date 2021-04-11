181d9203d126750fb484062e8eb881e15f447aa0
Artem Zymovets <artem.zimovets@gmail.com>
2021-04-11 17:35:27 +0300

    changes

--- services/email.js
+++ services/email.js
@@ -1,20 +1,20 @@
-const sgMail = require("@sendgrid/mail");
-const Mailgen = require("mailgen");
-const config = require("../config/email.json");
-require("dotenv").config();
+const sgMail = require('@sendgrid/mail');
+const Mailgen = require('mailgen');
+const config = require('../config/email.json');
+require('dotenv').config();
 class EmailService {
   #sender = sgMail;
   #GenerateTemplate = Mailgen;
 
   constructor(env) {
     switch (env) {
-      case "development":
+      case 'development':
         this.link = config.dev;
         break;
-      case "stage":
+      case 'stage':
         this.link = config.stage;
         break;
-      case "production":
+      case 'production':
         this.link = config.prod;
         break;
       default:
@@ -25,7 +25,7 @@ class EmailService {
 
   #createTemplate(body) {
     const mailGenerator = new this.#GenerateTemplate({
-      theme: "neopolitan",
+      theme: 'neopolitan',
       product: {
         name: body.name,
         link: this.link,
@@ -38,7 +38,7 @@ class EmailService {
         table: {
           data: body.questions,
         },
-        outro: "Thank you for your answers!",
+        outro: 'Thank you for your answers!',
       },
     };
     return mailGenerator.generate(template);
@@ -76,22 +76,21 @@ class EmailService {
   // const email = 'artem.zimovets@gmail.com';
 
   async sendEmail(email, body) {
-
     // TODO count score
     // подготовить answers
 
-    const emailBody = this.#createTemplate(email, body);
+    const emailBody = this.#createTemplate(body);
     this.#sender.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: email,
 
-      from: "artwayprojects@gmail.com",
+      from: 'artwayprojects@gmail.com',
 
       subject: `Your score is ${body.total} %!`,
       html: emailBody,
     };
     await this.#sender.send(msg);
-    require("fs").writeFileSync("./public/email.html", emailBody, "utf8");
+    require('fs').writeFileSync('./public/email.html', emailBody, 'utf8');
   }
 }
 
