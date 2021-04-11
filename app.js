6df63b90c8ab7cb7076c091ca1aaca120fd86b7b
Vladyslav <56783517+VVGhost5@users.noreply.github.com>
2021-04-11 12:19:54 +0300

    Swagger (#13)
    * Update README.md
    
    * initial commit
    
    * added swagger, not work queries
    
    * swagger done
    
    Co-authored-by: Половинка Иван <62117182+transformator98@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 // const path = require('path');
 
 // const { limiter } = require("./helpers/rate-limit");
-const { httpCode } = require('./helpers/constants');
+const { httpCode } = require("./helpers/constants");
 
-require('dotenv').config();
+require("dotenv").config();
+
 
 const authRouter = require('./routes/api/auth');
 const testsRouter = require('./routes/tests');
 const emailsRouter = require('./routes/emails');
 
 const app = express();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(helmet());
-app.get('env') !== 'test' && app.use(logger(formatsLogger));
+app.get("env") !== "test" && app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 // app.use(limiter);
 
-app.use('/auth', authRouter);
-app.use('/tests', testsRouter);
+app.use("/auth", authRouter);
+app.use("/tests", testsRouter);
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/emails', emailsRouter);
 
 app.use((req, res) => {
-  res.status(httpCode.NOT_FOUND).json({ message: 'Not found' });
+  res.status(httpCode.NOT_FOUND).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
