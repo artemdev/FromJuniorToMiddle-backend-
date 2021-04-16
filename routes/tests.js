1ae440bcbddf8a87867419b2523bea52d3e2bcfe
Valentyna-Chudik <valentyna_chudik@ukr.net>
2021-04-16 11:36:24 +0300

    controllers and routes fixed

--- routes/tests.js
+++ routes/tests.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const guard = require('../helpers/guard');
+const guard = require("../helpers/guard");
 const {
   createTechResult,
   getTechResult,
   removeResult,
   createTheoryResult,
   getTheoryResult,
-} = require('../controllers/tests');
+} = require("../controllers/tests");
 
-const path = require('path');
-const { listRandomTests } = require('../helpers/parseQuestions');
+const path = require("path");
+const { listRandomTests } = require("../helpers/parseQuestions");
 
-const testingTheory = path.join(__dirname, '../db/testingTheory.json');
-const technicalQA = path.join(__dirname, '../db/technicalQA.json');
+const testingTheory = path.join(__dirname, "../db/testingTheory.json");
+const technicalQA = path.join(__dirname, "../db/technicalQA.json");
 
 /* GET users listing. */
-router.get('/technical/random', async (_req, res, next) => {
+router.get("/technical/random", async (_req, res, next) => {
   try {
     const tests = await listRandomTests(technicalQA);
     return res.json({
-      status: 'success',
+      status: "success",
       code: 200,
       data: {
         tests,
@@ -32,11 +32,11 @@ router.get('/technical/random', async (_req, res, next) => {
   }
 });
 
-router.get('/theory/random', async (_req, res, next) => {
+router.get("/theory/random", async (_req, res, next) => {
   try {
     const tests = await listRandomTests(testingTheory);
     return res.json({
-      status: 'success',
+      status: "success",
       code: 200,
       data: {
         tests,
@@ -47,24 +47,11 @@ router.get('/theory/random', async (_req, res, next) => {
   }
 });
 
-// router.get('/technicalQA/result', guard, getTechResult);
-router.post('/technicalQA/result', guard, createTechResult);
-router.delete('/technicalQA/result', guard, removeResult);
-
-// router.get('/testingTheory/result', guard, getTheoryResult);
-router.post('/testingTheory/result', guard, createTheoryResult);
-router.delete('/testingTheory/result', guard, removeResult);
-// better names
-// router.post('/technical', guard, createTechResult);
-router.post('/technical', guard, createTechResult);
-router.post('/theory', guard, createTheoryResult);
-router.get('/technical', guard, getTechResult);
-router.get('/theory', guard, getTheoryResult);
-router.delete('/technical', guard, removeResult);
-router.delete('/theory', guard, removeResult);
-
-// router.post('/theory', guard, createTheoryResult);
-
-// router.delete('/theory', guard, removeResult);
+router.post("/technical", guard, createTechResult);
+router.post("/theory", guard, createTheoryResult);
+router.get("/technical", guard, getTechResult);
+router.get("/theory", guard, getTheoryResult);
+router.delete("/technical", guard, removeResult);
+router.delete("/theory", guard, removeResult);
 
 module.exports = router;
